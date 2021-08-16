edcebfc38ae5dad32f23f29e77141fa395de50a2
unknown <lolaazizova082@gmail.com>
2021-08-16 20:50:29 +0600

    added new models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ToDo
+from .models import ToMeet
+
+
+admin.site.register(ToDo)
+admin.site.register(ToMeet)
 
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.fields import BooleanField
 
-# Create your models here.
+class ToDo(models.Model):
+   text = models.CharField(max_length=100) 
+   created_at = models.DateField(auto_now_add=True)
+   is_closed = models.BooleanField(default=False)
+   is_favorite = models.BooleanField(default=False)
+
+
+
+class ToMeet(models.Model):
+
+    persone = models.CharField(max_length=100) 
+    phone_number = models.CharField(max_length=40)
+    date_of_meeting = models.DateTimeField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
