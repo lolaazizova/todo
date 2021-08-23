a7903abbe3053b7489049278d70560e26a8f7f0c
unknown <lolaazizova082@gmail.com>
2021-08-23 17:29:00 +0600

    added todo mark and unmark

--- todo/urls.py
+++ todo/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path("habit/", habit),
     path("add-todo/", add_todo, name="add-todo"),
     path("delete-todo/<id>/", delete_todo, name="delete-todo"),
-    
+     path("mark-todo/<id>/", mark_todo, name="mark-todo"),
+    path("unmark-todo/<id>/", unmark_todo, name="unmark-todo"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_roots=settings.MEDIA_ROOT)
